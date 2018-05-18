70c05859bcf88394fc150f1e1711c9e0ff8cfedf
Val Neekman <val@neekware.com>
2018-05-18 11:19:14 -0400

    clean up

--- apps/web/src/environments/environment.ts
+++ apps/web/src/environments/environment.ts
@@ -3,7 +3,7 @@ import { LogLevels } from '@nwx/logger';
 
 export const environment: AppCfg = {
   // app name
-  appName: '@nwx/jwt',
+  appName: '@nwx/menu',
   // target (browser, mobile, desktop)
   target: TargetPlatform.web,
   // production, staging or development
@@ -12,10 +12,4 @@ export const environment: AppCfg = {
     // log level (application-wide)
     level: LogLevels.debug
   },
-  jwt: {
-    // estimate time of http request between client -> server (greater than zero)
-    networkDelay: 1,
-    // few seconds to make the randomizer work. backend can overwrite
-    expiryLeeway: 5
-  }
 };
